e9fcea20e5688684505dd5b400adeb8c0949bacc
Jurema1988 <barroso.juh@gmail.com>
2025-06-05 01:12:22 +0000

    Desafio Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
     char estado1 = 'A';
     const char* codigodacarta1 = "A01";
     const char* nomedacidade1 = "Rio_Branco";
-    int população1 = 880631;
+    int populacao1 = 880631;
     float area1 = 164173.525;
     float PIB1 = 23.6;
     int numerodepontosturisticos1 = 9;
@@ -17,7 +17,7 @@ int main() {
     char estado2 = 'B';
     const char* codigodacarta2 = "B01";
     const char* nomedacidade2 = "Salvador";
-    int população2 = 14141626;
+    int populacao2 = 14141626;
     float area2 = 564760.429;
     float PIB2 = 115.9;
     int numerodepontosturisticos2 = 23;
@@ -27,7 +27,7 @@ int main() {
     printf("Estado: %c \n", estado1);
     printf("Código: %s \n", codigodacarta1);
     printf("Nome da Cidade: %s \n", nomedacidade1);
-    printf("População: %d \n", população1);
+    printf("População: %d \n", populacao1);
     printf("Àrea: %.2f em KM²\n", area1);
     printf("PIB: %.2f bilhões de reais \n", PIB1);
     printf("Número de pontos Turísticos: %d \n\n", numerodepontosturisticos1);
@@ -37,7 +37,7 @@ int main() {
     printf("Estado: %c \n", estado2);
     printf("Código: %s \n", codigodacarta2);
     printf("Nome da Cidade: %s \n", nomedacidade2);
-    printf("População: %d \n", população2);
+    printf("População: %d \n", populacao2);
     printf("Àrea: %.2f em KM²\n", area2);
     printf("PIB: %.2f bilhões de reais \n", PIB2);
     printf("Número de pontos Turísticos: %d \n\n", numerodepontosturisticos2);
